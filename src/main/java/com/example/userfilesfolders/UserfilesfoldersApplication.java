9d5c5907295f14a8bcf18b569776523f1ddc476f
Gerry Waterston <gwaterston@hotmail.co.uk>
2019-02-26 15:59:08 +0000

    Create project

---
+++ src/main/java/com/example/userfilesfolders/UserfilesfoldersApplication.java
@@ -0,0 +1,13 @@
+package com.example.userfilesfolders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserfilesfoldersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserfilesfoldersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userfilesfolders/UserfilesfoldersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.userfilesfolders;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserfilesfoldersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
